4b4a9e0b2825ce10408f1f70877c43b9d210a4ea
Jason Zhao Jie <261343578@qq.com>
2019-04-28 21:01:05 +0800

    Add SQL classes

---
+++ IO/ISqlClient.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArmyAnt.IO {
+    public abstract class ISqlClient {
+        public abstract bool Connect(string serverHost);
+        public abstract bool Connect(string serverAddress, string port);
+        public abstract void Disconnect();
+        public abstract bool IsConnection();
+        public abstract uint GetDatabaseCount();
+        public abstract string[] GetDatabaseList();
+
+        public abstract long GetTablesCount();
+        public abstract long GetViewsCount();
+        public abstract string[] GetTableNameList();
+        public abstract string[] GetViewNameList();
+
+        public SqlTable GetWholeTable(string tableName) => Select(tableName);
+        public SqlTable GetWholeView(string tableName) => Select(tableName);
+        public abstract string[] getTableAllFields(string table);
+
+        // select * from [tableName]
+        public SqlTable Select(string tableName, params SqlClause[] clauses) => Query("select * from " + tableName + OrganizeSqlClause(clauses));
+        // select [columnNames] from [tableName]
+        public SqlTable Select(string tableName, string[] columnNames, params SqlClause[] clauses) {
+            string sql = "select ";
+            if(columnNames != null)
+                for(int i = 0; i < columnNames.Length; ++i) {
+                    sql += columnNames[i] + " , ";
+                }
+            return Query(sql + "from " + tableName + OrganizeSqlClause(clauses));
+        }
+        // update [tableName] set [updatedData ( k=value , k=value ... )]
+        public long Update(string tableName, IEnumerable<SqlField> data, params SqlClause[] clauses) {
+            string sql = "update " + tableName + " set ";
+            foreach(var i in data) {
+                sql += i.Head.columnName + " = \"" + i.Value + "\" " + ", ";
+            }
+            sql = sql.Remove(sql.Length - 2);
+            return Update(sql + OrganizeSqlClause(clauses));
+        }
+        // insert into [tableName] [insertedData (k , k , k ... ) values ( value , value , value ... )]
+        public long InsertRow(string tableName, IEnumerable<SqlField> data) {
+            string keys = "";
+            string values = "";
+            foreach(var i in data) {
+                keys += i.Head.columnName + ", ";
+                values += i.Value + ", ";
+            }
+            keys = keys.Remove(keys.Length - 2);
+            values = values.Remove(keys.Length - 2);
+
+            return Update("insert into " + tableName + " ( " + keys + " ) values ( " + values + " )");
+        }
+        // alter table [tableName] add [columnHead name dataType (others)...]
+        public long InsertColumn(string tableName, params SqlFieldHead[] columnHead) => Update("alter table " + tableName + " add " + OrganizeColumnInfo(columnHead));
+        // delete from [tableName]
+        public long DeleteRow(string tableName, SqlClause where = null) {
+            if(where != null && where.Type != SqlClauseType.Where)
+                return -1;
+            return Update("delete from " + tableName + ' ' + OrganizeSqlClause(where));
+
+        }
+        // alter table [tableName] drop column [columnName]
+        public long DeleteColumn(string tableName, string columnName) => Update("alter table " + tableName + " drop column " + columnName);
+
+        public long CreateDatabase(string dbName) => Update("create database " + dbName);
+        public long DeleteDatabase(string dbName) => Update("drop database " + dbName);
+        public long CreateTable(SqlTable table) {
+            string sql = "create table " + table.tableName + " ( ";
+            for(int i = 0; i < table.Width; ++i) {
+                sql += OrganizeColumnInfo(table.GetHead(i)) + " , ";
+            }
+            sql = sql.Remove(sql.Length - 2);
+            return Update(sql + " )");
+        }
+        public long DeleteTable(string tableName) => Update("drop table " + tableName);
+
+        public string OrganizeColumnInfo(params SqlFieldHead[] column) {
+            // TODO: 应当扩展支持的更多属性
+            string ret = "";
+            foreach(var i in column) {
+                ret += i.columnName + ' ' + i.type.ToString() + (i.allowNull ? "" : " not null") + ", ";
+            }
+            ret = ret.Remove(ret.Length - 2);
+            return ret;
+        }
+
+        public string OrganizeSqlClause(params SqlClause[] clauses) {
+            // TODO: 完善clause的结构然后补全此处
+            string sql = "";
+
+            return sql;
+        }
+
+        public abstract SqlTable Query(string sql);
+        public abstract long Update(string sql);
+        // Call 'excute' to execute multi-result sql, if the result param was run, the sql will still be executed
+        // 调用驱动的execute执行可以返回多个结果集的SQL语句, 如果传入的result参数不可用, sql语句仍将被执行, 且结果仍将返回结果集个数. 
+        // 只有sql语句执行失败才会返回负数. 如果执行update语句, 则不会返回任何结果, 但返回值会返回影响的行数 (同update)
+        public abstract SqlTable[] Execute(string sql);
+    }
+}
---
+++ IO/SqlStructures.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArmyAnt.IO {
+    // 数据库字段类型, 不同的数据库, 类型不同
+    public enum SqlFieldType : byte {
+        Unknown,
+        Null,
+        UpdateResult,
+
+        MySql_BIT,
+        MySql_CHAR,
+        MySql_VARCHAR,
+        MySql_ENUM,
+        MySql_SET,
+        MySql_TINYINT,
+        MySql_SMALLINT,
+        MySql_MEDIUMINT,
+        MySql_INT,
+        MySql_BIGINT,
+        MySql_FLOAT,
+        MySql_DOUBLE,
+        MySql_DEMICAL,
+        MySql_NUMERIC,
+        MySql_DATE,
+        MySql_DATETIME,
+        MySql_TIMESTAMP,
+        MySql_TIME,
+        MySql_YEAR,
+        MySql_GEOMETRY,
+        MySql_BINARY,
+        MySql_VARBINARY,
+        MySql_LONGVARCHAR,
+        MySql_LONGVARBINARY,
+        MySql_JSON,
+
+        MsAccess_Currency,
+        MsAccess_AutoNumber,
+        MsAccess_YesNo,
+        MsAccess_Hyperlink,
+        MsAccess_Text,// = MySql_Varchar,
+        MsAccess_Memo,// = MySql_Text,
+        MsAccess_Byte,// = MySql_TinyInt,
+        MsAccess_Integer,// = MySql_SmallInt,
+        MsAccess_Long,// = MySql_Int,
+        MsAccess_Single,// = MySql_Float,
+        MsAccess_Double,// = MySql_Double,
+        MsAccess_DateTime,// = MySql_DateTime,
+        MsAccess_OleObject,// = MySql_LongBlob,
+        MsAccess_LookupWizard,// = MySql_Enum,
+
+        MsSqlServer_char,
+        MsSqlServer_varchar,
+        MsSqlServer_text,
+        MsSqlServer_nchar,
+        MsSqlServer_nvarchar,
+        MsSqlServer_ntext,
+        MsSqlServer_bit,
+        MsSqlServer_binary,
+        MsSqlServer_varbinary,
+        MsSqlServer_image,
+        MsSqlServer_tinyint,
+        MsSqlServer_smallint,
+        MsSqlServer_int,
+        MsSqlServer_bigint,
+        MsSqlServer_decimal,
+        MsSqlServer_numeric,
+        MsSqlServer_smallmoney,
+        MsSqlServer_money,
+        MsSqlServer_float,
+        MsSqlServer_real,
+        MsSqlServer_datetime,
+        MsSqlServer_datetime2,
+        MsSqlServer_smalldatetime,
+        MsSqlServer_date,
+        MsSqlServer_time,
+        MsSqlServer_datetimeoffset,
+        MsSqlServer_timestamp,
+        MsSqlServer_sql_variant,
+        MsSqlServer_uniqueidentifier,
+        MsSqlServer_xml,
+        MsSqlServer_cursor,
+        MsSqlServer_table,
+
+        MsExcel_Normal,
+    };
+
+    // 条件语句类型
+    public enum SqlClauseType {
+        Null,
+        Where,
+        OrderBy,
+        Top
+    };
+
+    // 运算符类型
+    public enum SqlOperatorType {
+        none,
+        _is,
+        like,
+        _in,
+        between,
+        and,
+        or,
+        alias,
+        innerJoin,
+        leftJoin,
+        rightJoin,
+        fullJoin
+    };
+
+    // 表头信息结构
+    public struct SqlFieldHead {
+        public uint length;
+        public string catalogName;
+        public string columnName;
+        public SqlFieldType type;
+        public bool allowNull;
+        public bool autoIncrease;
+    };
+
+    // 单字段类
+    public class SqlField {
+        public SqlField() {
+            Head = default;
+            Value = "";
+        }
+        public SqlField(string value, SqlFieldHead head) {
+            Head = head;
+            Value = value;
+        }
+
+        public SqlFieldHead Head { get; private set; }
+        public string Value { get; set; }
+    };
+
+    // 数据库信息结构
+    public struct SqlDatabaseInfo {
+        public string name;
+        public string server;
+        public string charset;
+        public string sortRule;
+    };
+
+    // 表信息结构
+    public class SqlTableInfo {
+        public string tableName;
+        public string engine;
+        public string comment;
+        public SqlDatabaseInfo[] parentDatabase;
+    };
+
+    // 表类
+    public class SqlTable : SqlTableInfo {
+        public SqlTable(IEnumerable<SqlFieldHead> heads) {
+            this.heads = new List<SqlFieldHead>();
+            fields = new List<List<SqlField>>();
+            foreach(var i in heads) {
+                this.heads.Add(i);
+            }
+        }
+        public int Width => heads.Count;
+        public int Height => fields.Count;
+        public SqlFieldHead GetHead(int index) => heads[index];
+        public IList<SqlField> GetRow(int index) => fields[index];
+        public void AddRow(List<SqlField> rowFields) => fields.Add(rowFields);
+
+        readonly IList<SqlFieldHead> heads;
+        readonly IList<List<SqlField>> fields;
+    };
+
+    // SQL表达式类
+    public class SqlExpress {
+        public SqlExpress(string str = "") {
+            PushValue(str);
+        }
+
+        public bool PushValue(params string[] value) {
+            // TODO
+            return false;
+        }
+        public bool RemoveValue(uint index) {
+            // TODO
+            return false;
+        }
+        public bool Clear() {
+            // TODO
+            return false;
+        }
+        public SqlOperatorType Type { get; set; }
+    };
+
+    // SQL条件语句类
+    public class SqlClause {
+        public SqlClause(string str = "") {
+            PushExpress(new SqlExpress(str));
+        }
+
+        public bool PushExpress(params SqlExpress[] value) {
+            // TODO:
+            return false;
+        }
+        public bool RemoveExpress(uint index) {
+            // TODO: 
+            return false;
+        }
+        public bool Clear() {
+            // TODO:
+            return false;
+        }
+        public SqlClauseType Type { get; set; }
+    };
+
+}
