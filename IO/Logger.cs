62043cc894abade379f8294afeadb57b10111690
Jason Zhao Jie <261343578@qq.com>
2019-04-28 14:46:29 +0800

    Add log level and time string to logger

--- IO/Logger.cs
+++ IO/Logger.cs
@@ -5,38 +5,65 @@ using System.Text;
 
 namespace ArmyAnt.IO {
     public class Logger : WriteOnlyStream {
+        public enum LogLevel {
+            Verbose,
+            Debug,
+            Info,
+            Import,
+            Warning,
+            Error,
+            Fatal,
+        }
+        public static LogLevel LevelFromString(string str) {
+            foreach(var i in Enum.GetValues(typeof(LogLevel))) {
+                if(i.ToString().ToLower() == str) {
+                    return (LogLevel)i;
+                }
+            }
+            return default;
+        }
+
         /// <summary> 能否写, 恒返回true </summary>
         public override bool CanWrite => true;
         public Encoding DefaultWritingEncoding { get; set; }
 
-        public Logger(bool withConsole = true) {
+        public Logger(bool withConsole = true, LogLevel consoleLevel = LogLevel.Info, bool consoleTimestamp = false) {
             DefaultWritingEncoding = Encoding.UTF8;
             if(withConsole) {
                 var console = Console.OpenStandardOutput();
                 if(console != null && console.CanWrite) {
-                    AddStream(console);
+                    AddStream(console, consoleLevel, consoleTimestamp);
                 }
             }
         }
 
         /// <summary>
-        /// 写入二进制数据
+        /// 写入自定义不加前缀的二进制数据
         /// </summary>
         /// <param name="buffer"> 要写入的数据 </param>
         /// <param name="offset"> 可选参数, 要从 buffer 的哪个起始位置开始写入 </param>
         /// <param name="count"> 可选参数,写入多长的数据 </param>
         public override void Write(byte[] buffer, int offset = 0, int count = 0) {
+            Write(LogLevel.Verbose, buffer, offset, count);
+        }
+        public void Write(LogLevel level, byte[] buffer, int offset = 0, int count = 0) {
             if(count == 0) {
                 count = buffer.Length;
             }
+            var timeBytes = Encoding.Default.GetBytes("[ " + DateTime.Now.ToString() + " ] ");
+            var levelBytes = Encoding.Default.GetBytes("[ " + level.ToString() + " ] ");
             lock(streams) {
                 foreach(var i in streams) {
-                    if(i != null && i.CanWrite) {
-                        if(i.CanSeek) {
-                            i.Seek(0, SeekOrigin.End);
+                    if(i.stream != null && i.stream.CanWrite && i.level <= level) {
+                        if(i.stream.CanSeek) {
+                            i.stream.Seek(0, SeekOrigin.End);
                         }
-                        i.Write(buffer, offset, count);
-                        i.Flush();
+                        if(i.timeStamp) {
+                            i.stream.Write(timeBytes, 0, timeBytes.Length);
+                        }
+                        i.stream.Write(levelBytes, 0, levelBytes.Length);
+                        i.stream.Write(buffer, offset, count);
+                        i.stream.Flush();
                     } else {
                         streams.Remove(i);
                     }
@@ -45,45 +72,44 @@ namespace ArmyAnt.IO {
         }
 
         /// <summary>
-        /// 写入一个 默认编码的 字符串
-        /// </summary>
-        /// <param name="content"> 要写入的内容 </param>
-        public void Write(string content) {
-            Write(content, DefaultWritingEncoding);
-        }
-
-        /// <summary>
         /// 写入一个指定编码的字符串
         /// </summary>
         /// <param name="content"> 要写入的内容 </param>
         /// <param name="encodeType"> 要写入的字符串编码 </param>
-        public void Write(string content, Encoding encodeType) {
+        public void Write(LogLevel level, string content, Encoding encodeType) {
             var bts = encodeType.GetBytes(content);
-            Write(bts, 0, bts.Length);
+            Write(level, bts);
+        }
+
+        /// <summary>
+        /// 写入一个 默认编码的 字符串
+        /// </summary>
+        /// <param name="content"> 要写入的内容 </param>
+        public void Write(LogLevel level, string content) {
+            Write(level, content, DefaultWritingEncoding);
         }
 
         /// <summary>
         /// 写入一组对象拼接成的字符串
         /// </summary>
-        /// <param name="content"> 要写入的第一个对象, 必须至少有一个 </param>
-        /// <param name="strings"> 要写入的后续对象, 所有这些对象会被转化为字符串写入 </param>
-        public void Write(object content, params object[] strings) {
+        /// <param name="strings"> 要写入的对象, 所有这些对象会被转化为字符串写入 </param>
+        public void Write(LogLevel level, string content, params object[] strings) {
             if(strings == null || strings.Length == 0) {
-                Write(content.ToString());
+                return;
             }
-            var str = new StringBuilder(content.ToString());
+            var str = new StringBuilder(content);
             foreach(var i in strings) {
                 str.Append(strings);
             }
-            Write(str.ToString());
+            Write(level, str.ToString());
         }
 
         /// <summary>
         /// 写入一个字符串并自动换行
         /// </summary>
         /// <param name="content"></param>
-        public void WriteLine(string content) {
-            Write(content + Environment.NewLine);
+        public void WriteLine(LogLevel level, string content) {
+            Write(level, content + Environment.NewLine);
         }
 
         /// <summary>
@@ -91,16 +117,15 @@ namespace ArmyAnt.IO {
         /// </summary>
         /// <param name="content"> 要写入的第一个对象, 必须至少有一个 </param>
         /// <param name="strings"> 要写入的后续对象, 所有这些对象会被转化为字符串写入 </param>
-        public void WriteLine(object content, params object[] strings) {
-            Write(content, strings);
-            Write(Environment.NewLine);
+        public void WriteLine(LogLevel level, string content, params object[] strings) {
+            Write(level, content, strings, Environment.NewLine);
         }
 
         /// <summary>
         /// 添加一个输出流, 写操作会写入到这些流
         /// </summary>
         /// <param name="stream"> 要添加的输出流, 必须是可写的 </param>
-        public void AddStream(Stream stream) {
+        public void AddStream(Stream stream, LogLevel level, bool timeStamp) {
             if(stream == null) {
                 throw new ArgumentNullException();
             }
@@ -108,7 +133,7 @@ namespace ArmyAnt.IO {
                 throw new ArgumentException("The adding stream cannot write");
             }
             lock(streams) {
-                streams.Add(stream);
+                streams.Add(new StreamInfo() { stream = stream, level = level, timeStamp = timeStamp});
             }
         }
 
@@ -122,8 +147,16 @@ namespace ArmyAnt.IO {
                 throw new ArgumentNullException();
             }
             lock(streams) {
-                return streams.Remove(stream);
+                StreamInfo ret = default;
+                foreach(var i in streams) {
+                    if(i.stream == stream) {
+                        ret = i;
+                    }
+                }
+                if(ret.stream == stream)
+                    return streams.Remove(ret);
             }
+            return false;
         }
 
         /// <summary>
@@ -143,6 +176,12 @@ namespace ArmyAnt.IO {
             return File.Open(str.ToString(), FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
         }
 
-        private IList<Stream> streams = new List<Stream>();
+        private struct StreamInfo {
+            public Stream stream;
+            public LogLevel level;
+            public bool timeStamp;
+        }
+
+        private IList<StreamInfo> streams = new List<StreamInfo>();
     }
 }
