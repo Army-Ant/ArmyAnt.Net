f52f94e10394a9b79cb827532df1ff62bc3fb334
Jason Cho <261343578@qq.com>
2023-09-11 12:17:47 +0800

    添加 EventManager 工具接口，具体实现由平台库决定

---
+++ ProcessController/IEventManager.cs
@@ -0,0 +1,12 @@
+﻿namespace ArmyAnt.ProcessController {
+    public interface IEventManager<T_EventID> {
+        public interface IEventArgs {
+            T_EventID EventId { get; }
+        }
+
+        int Listen(T_EventID eventId, System.Action<IEventArgs> callback);
+        bool Unlisten(T_EventID eventId, int listenerId);
+        bool NotifyAsync(IEventArgs args);
+        void NotifySync(IEventArgs args);
+    }
+}
